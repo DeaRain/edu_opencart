11b93545b422e02eebe5fb5f0d8d84ca3b6b76eb
DeaRain <steallmaster@gmail.com>
2019-08-01 12:14:40 +0300

    test

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test.loc/admin/');
+define('HTTP_CATALOG', 'http://test.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test.loc/admin/');
+define('HTTPS_CATALOG', 'http://test.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/test.loc/html/admin/');
+define('DIR_SYSTEM', '/var/www/test.loc/html/system/');
+define('DIR_LANGUAGE', '/var/www/test.loc/html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/test.loc/html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/test.loc/html/system/config/');
+define('DIR_IMAGE', '/var/www/test.loc/html/image/');
+define('DIR_CACHE', '/var/www/test.loc/html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/test.loc/html/system/storage/download/');
+define('DIR_LOGS', '/var/www/test.loc/html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/test.loc/html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/test.loc/html/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/test.loc/html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dearain');
+define('DB_PASSWORD', 'allhimik');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/test.loc/html/catalog/');
+define('DIR_SYSTEM', '/var/www/test.loc/html/system/');
+define('DIR_LANGUAGE', '/var/www/test.loc/html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/test.loc/html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/test.loc/html/system/config/');
+define('DIR_IMAGE', '/var/www/test.loc/html/image/');
+define('DIR_CACHE', '/var/www/test.loc/html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/test.loc/html/system/storage/download/');
+define('DIR_LOGS', '/var/www/test.loc/html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/test.loc/html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/test.loc/html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dearain');
+define('DB_PASSWORD', 'allhimik');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
